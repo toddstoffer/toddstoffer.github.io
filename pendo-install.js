69f2c1f5de84ba051ed919768f36285b5ad4b84d
Todd Stoffer <toddstoffer+github@gmail.com>
2023-11-07 15:38:26 -0500

    adding segment install

--- pendo-install.js
+++ pendo-install.js
@@ -38,3 +38,29 @@
         });
 
 })('5ba2118f-b462-47f9-66eb-e0eeddbf109d');
+
+
+
+
+//Segment.com Install 
+
+
+//   !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){if(window.analytics.initialized)return window.analytics[e].apply(window.analytics,arguments);var i=Array.prototype.slice.call(arguments);i.unshift(e);analytics.push(i);return analytics}};for(var i=0;i<analytics.methods.length;i++){var key=analytics.methods[i];analytics[key]=analytics.factory(key)}analytics.load=function(key,i){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=i};analytics._writeKey="VXrEPSlhnbVxODjBTfrk0sbdU29jLcNd";;analytics.SNIPPET_VERSION="4.16.1";
+//   analytics.load("VXrEPSlhnbVxODjBTfrk0sbdU29jLcNd");
+//   analytics.page();
+//   analytics.identify("segment-user-id", {
+//     id: "Segment user",
+//     email: "peter@example.com",
+//     plan: "premium",
+//     logins: 5
+//   });
+//   analytics.group("segment-account-id", {
+//     name: "Segment Account"
+//   });
+//   }}();
+
+
+//GTM Install 
+
+
+
