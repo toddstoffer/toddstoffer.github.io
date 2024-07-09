a650ea3e589709fd77cf4f9ac8524c93f3547454
Todd Stoffer <toddstoffer+github@gmail.com>
2024-07-09 11:29:17 -0400

    Update pendo-install.js

--- pendo-install.js
+++ pendo-install.js
@@ -26,7 +26,7 @@
                 // is_paying:       true,
                 // tags:           ['enterprise'],
             } ,
-            cookieDomain: '*.github.io'
+            cookieDomain: '.github.io'
 //<--Set Location API-->
             // location: {
             //     transforms: [annotateAddTransform]
